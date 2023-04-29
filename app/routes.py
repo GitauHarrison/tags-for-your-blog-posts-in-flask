59f348cda8a87e94d9e31b5392eb9175d7f3bdd4
harry <harrisongitau3@gmail.com>
2023-04-29 15:25:09 +0300

    Style: Use tailwindcss

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,18 @@
+from flask import render_template, url_for, session, redirect
 from app import app
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return 'Hello, world!'
+    return render_template(
+        'index.html',
+        title='Post Something'
+    )
+
+@app.route('/blogs-by-tags')
+def blogs_by_tags():
+    return render_template(
+        'blogs_by_tags.html',
+        title='Blogs Based On'
+    )
